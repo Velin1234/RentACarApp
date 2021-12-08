7edec8588dfd67334a6ed519bfe159ce1ae3c3c9
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-12-08 10:49:48 +0200

    Initial commit

---
+++ RentACar/Data/RentACarDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RentACar.Models;
+
+namespace RentACar.Data
+{
+    public class RentACarDbContext : DbContext
+    {
+        public RentACarDbContext (DbContextOptions<RentACarDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RentACar.Models.Car> Cars { get; set; }
+    }
+}
