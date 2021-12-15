f55bb6e39e8e13cc14c6fbf242bdc3680ab3f103
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-12-15 23:44:28 +0200

    Added more Admin refinements

---
+++ RentACarApplication/Data/Migrations/20211215205018_AddAdminAccount.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+namespace RentACarApplication.Data.Migrations
+{
+    public partial class AddAdminAccount : Migration
+    {
+        private const string ADMIN_USER_GUID = "b9b241c5-638f-4ffd-bf51-439829a9ef58";
+        private const string ADMIN_ROLE_GUID = "cb048b90-9e8a-49da-a3d8-6fed27fb9b6b";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+            var passwordHash = hasher.HashPassword(null,"AdminPassword");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("INSERT INTO AspNetUsers(Id, UserName, NormalizedUserName, Email, EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled,AccessFailedCount,NormalizedEmail,PasswordHash,SecurityStamp,FirstName)");
+            sb.AppendLine("VALUES(");
+            sb.AppendLine($"'{ADMIN_USER_GUID}'");
+            sb.AppendLine(",'admin@rentacar.co.bg'");
+            sb.AppendLine(",'ADMIN@RENTACAR.CO.BG'");
+            sb.AppendLine(",'admin@rentacar.co.bg'");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(",'ADMIN@RENTACAR.CO.BG'");
+            sb.AppendLine($", '{passwordHash}'");
+            sb.AppendLine(", ''");
+            sb.AppendLine(", 'Admin'");
+            sb.AppendLine(")");
+
+            migrationBuilder.Sql(sb.ToString());
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id,Name,NormalizedName) VALUES('{ADMIN_ROLE_GUID}','Admin','ADMIN')");
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId,RoleId) VALUES('{ADMIN_USER_GUID}','{ADMIN_ROLE_GUID}')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetUserRoles WHERE UserId = '{ADMIN_USER_GUID}' AND RoleId = '{ADMIN_ROLE_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetUsers WHERE Id = '{ADMIN_USER_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = '{ADMIN_ROLE_GUID}'");
+        }
+    }
+}
