e925e421ddf6953fc86ab2641f6079a7f9a6454a
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-12-18 13:35:32 +0200

    Added custom login dialog

---
+++ RentACarApplication/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACarApplication.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [StringLength(100,MinimumLength = 2)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Запомни ме")]
+        public bool RememberMe { get; set; }
+        public string LoginInValid { get; set; }
+    }
+}
